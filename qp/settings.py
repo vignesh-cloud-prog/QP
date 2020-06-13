e3e39c786370810236899fd12c47e2ea1e8313b9
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-13 12:26:22 +0530

    heroku changes

--- qp/settings.py
+++ qp/settings.py
@@ -148,11 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+# ]
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
