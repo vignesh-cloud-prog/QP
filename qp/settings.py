68c16290154bb02859332438b14ebf77d24a09e2
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-14 09:24:39 +0530

    first commit

--- qp/settings.py
+++ qp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2bzp&z&ou_i-dw^b-wy6cv+m##8!6rdcll9+%p0b-ar1gc^f+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['questionpaper.herokuapp.com','127.0.0.1']
 
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'qp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'qp',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'vigneshnu77',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432',
-    # }
-# }
+# 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qp',
+        'USER': 'postgres',
+        'PASSWORD': 'vigneshnu77',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 # DATABASES = {
     # 'default': {
         # 'ENGINE': 'django.db.backends.postgresql',
@@ -102,12 +102,12 @@ WSGI_APPLICATION = 'qp.wsgi.application'
     # }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 
 
