5a75c96fbf3d24d575a91bb8cb6f321d78a3501b
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-14 15:18:29 +0530

    heroku static

--- qp/settings.py
+++ qp/settings.py
@@ -156,10 +156,10 @@ STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+# ]
 # STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
