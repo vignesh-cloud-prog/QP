a536f5a530a955b2335e18b385f8902204f80cd7
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-14 14:52:00 +0530

    heroku static

--- qp/settings.py
+++ qp/settings.py
@@ -152,11 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+STATIC_URL = '/static/'
 # STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
