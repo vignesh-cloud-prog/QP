b128bb29d8902dbe11e11958ef0188cfba2547ed
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-14 09:49:32 +0530

    added database conn

--- qp/settings.py
+++ qp/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # DATABASES = {
     # 'default': {
         # 'ENGINE': 'django.db.backends.postgresql',
