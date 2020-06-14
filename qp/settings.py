b84833a2f519a6fb1b5ad471ab4e1777ac2ae36a
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-14 15:28:53 +0530

    last commit of static

--- qp/settings.py
+++ qp/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
