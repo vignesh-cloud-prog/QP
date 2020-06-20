467d15d22883c12b1e3c87e33de93dcc6aad24ec
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-06-20 09:12:16 +0530

    media

--- qp/settings.py
+++ qp/settings.py
@@ -164,7 +164,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
