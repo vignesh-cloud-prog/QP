27ee1f423d8e3dc1b4c5f96bffc302fda4151895
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-12-24 23:02:37 +0530

    commit changes
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- qp/settings.py
+++ qp/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST_PASSWORD='vigneshqpweb'
 # STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
