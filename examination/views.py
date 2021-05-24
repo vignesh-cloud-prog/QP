a690a66d6741af49305236cbda6b016759590612
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-05-24 21:45:37 +0530

    deleted some unused code
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- examination/views.py
+++ examination/views.py
@@ -1,174 +1,176 @@
 import random
-from django.http import HttpResponse,JsonResponse
-
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
-from .models import UserOTP,Profile
+from .models import UserOTP, Profile
 from django.core.mail import send_mail
-from django.shortcuts import render,redirect
-# from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from question_papers.models import Provider
+from question_papers.models import Provider, Question_papers
 from .forms import CreateUserForm
 # Create your views here.
+
 
 def logoutUser(request):
     logout(request)
     return redirect("login")
 
-def signup(request,*args,**kwargs):
-	code=str(kwargs.get('ref_code'))
-	try:
-		profile=Profile.objects.get(code=code)
-		request.session['ref_profile']=profile.id
-		print('profile.id ',profile.id)
-		print('id',profile.id)
-	except:
-		pass
-	print(request.session.get_expiry_date())
-	
-	profil_id=request.session.get('ref_profile')
 
-	if request.method == 'POST':
-		get_otp = request.POST.get('otp') #213243 #None
+def signup(request, *args, **kwargs):
+    code = str(kwargs.get('ref_code'))
+    try:
+        profile = Profile.objects.get(code=code)
+        request.session['ref_profile'] = profile.id
+        print('profile.id ', profile.id)
+        print('id', profile.id)
+    except:
+        pass
+    print(request.session.get_expiry_date())
 
-		if get_otp is not None:
-			get_usr = request.POST.get('usr')
-			usr = User.objects.get(username=get_usr)
-			if int(get_otp) == UserOTP.objects.filter(user = usr).last().otp:
-				usr.is_active = True
-				usr.save()
-				messages.success(request, f'Account is Created For {usr.username}')
-				return redirect('login')
-			else:
-				messages.warning(request, f'You Entered a Wrong OTP')
-				return render(request, 'accounts/register.html', {'otp': True, 'usr': usr})
+    profil_id = request.session.get('ref_profile')
 
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			instance=form.save()
-			if profil_id is not None:
-				recomended_by_profile=Profile.objects.get(id=profil_id)
-				registered_user=User.objects.get(id=instance.id)
-				registered_profile=Profile.objects.get(user=registered_user)
-				registered_profile.recomended_by=recomended_by_profile.user
-				registered_profile.save()
+    if request.method == 'POST':
+        get_otp = request.POST.get('otp')  # 213243 #None
 
-			username = form.cleaned_data.get('username')
-			email = form.cleaned_data.get('email')
-			name = form.cleaned_data.get('name')
+        if get_otp is not None:
+            get_usr = request.POST.get('usr')
+            usr = User.objects.get(username=get_usr)
+            if int(get_otp) == UserOTP.objects.filter(user=usr).last().otp:
+                usr.is_active = True
+                usr.save()
+                messages.success(
+                    request, f'Account is Created For {usr.username}')
+                return redirect('login')
+            else:
+                messages.warning(request, f'You Entered a Wrong OTP')
+                return render(request, 'accounts/register.html', {'otp': True, 'usr': usr})
 
-			usr = User.objects.get(username=username)
-			usr.email = email
-			usr.first_name = name
-			# usr.last_name = name[1]
-			usr.is_active = False
-			usr.save()
-			usr_otp = random.randint(100000, 999999)
-			UserOTP.objects.create(user = usr, otp = usr_otp)
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            instance = form.save()
+            if profil_id is not None:
+                recomended_by_profile = Profile.objects.get(id=profil_id)
+                registered_user = User.objects.get(id=instance.id)
+                registered_profile = Profile.objects.get(user=registered_user)
+                registered_profile.recomended_by = recomended_by_profile.user
+                registered_profile.save()
 
-			mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            name = form.cleaned_data.get('name')
 
-			send_mail(
-				"Welcome to QP Web - Verify Your Email",
-				mess,
-				'qpcom80@gmail.com',
-				[usr.email],
-				fail_silently = True
-				)
+            usr = User.objects.get(username=username)
+            usr.email = email
+            usr.first_name = name
+            # usr.last_name = name[1]
+            usr.is_active = False
+            usr.save()
+            usr_otp = random.randint(100000, 999999)
+            UserOTP.objects.create(user=usr, otp=usr_otp)
 
-			return render(request, 'accounts/register.html', {'otp': True, 'usr': usr})
+            mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
 
-		
-	else:
-		form = CreateUserForm()
+            send_mail(
+                "Welcome to QP Web - Verify Your Email",
+                mess,
+                'qpcom80@gmail.com',
+                [usr.email],
+                fail_silently=True
+            )
 
-	return render(request, 'accounts/register.html', {'form':form})
+            return render(request, 'accounts/register.html', {'otp': True, 'usr': usr})
 
-def resend_otp(request,*args,**kwargs):
-	if request.method == "GET":
-		get_usr = request.GET['usr']
-		if User.objects.filter(username = get_usr).exists() and not User.objects.get(username = get_usr).is_active:
-			usr = User.objects.get(username=get_usr)
-			usr_otp = random.randint(100000, 999999)
-			UserOTP.objects.create(user = usr, otp = usr_otp)
-			mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
+    else:
+        form = CreateUserForm()
 
-			send_mail(
-				"Welcome to QP Web - Verify Your Email",
-				mess,
-				'qpcom80@gmail.com',
-				[usr.email],
-				fail_silently = False
-				)
-			return HttpResponse("Resend")
+    return render(request, 'accounts/register.html', {'form': form})
 
-	return HttpResponse("Can't Send ")
+
+def resend_otp(request, *args, **kwargs):
+    if request.method == "GET":
+        get_usr = request.GET['usr']
+        if User.objects.filter(username=get_usr).exists() and not User.objects.get(username=get_usr).is_active:
+            usr = User.objects.get(username=get_usr)
+            usr_otp = random.randint(100000, 999999)
+            UserOTP.objects.create(user=usr, otp=usr_otp)
+            mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
+
+            send_mail(
+                "Welcome to QP Web - Verify Your Email",
+                mess,
+                'qpcom80@gmail.com',
+                [usr.email],
+                fail_silently=False
+            )
+            return HttpResponse("Resend")
+
+    return HttpResponse("Can't Send ")
 
 
 def login_view(request):
-	if request.user.is_authenticated:
-		return redirect('profile')
-	if request.method == 'POST':
-		get_otp = request.POST.get('otp') #213243 #None
+    if request.user.is_authenticated:
+        return redirect('profile')
+    if request.method == 'POST':
+        get_otp = request.POST.get('otp')  # 213243 #None
 
-		if get_otp:
-			get_usr = request.POST.get('usr')
-			usr = User.objects.get(username=get_usr)
-			if int(get_otp) == UserOTP.objects.filter(user = usr).last().otp:
-				usr.is_active = True
-				usr.save()
-				login(request, usr)
-				return redirect('profile')
-			else:
-				messages.warning(request, f'You Entered a Wrong OTP')
-				return render(request, 'accounts/login.html', {'otp': True, 'usr': usr})
+        if get_otp:
+            get_usr = request.POST.get('usr')
+            usr = User.objects.get(username=get_usr)
+            if int(get_otp) == UserOTP.objects.filter(user=usr).last().otp:
+                usr.is_active = True
+                usr.save()
+                login(request, usr)
+                return redirect('profile')
+            else:
+                messages.warning(request, f'You Entered a Wrong OTP')
+                return render(request, 'accounts/login.html', {'otp': True, 'usr': usr})
 
+        usrname = request.POST['username']
+        passwd = request.POST['password']
 
-		usrname = request.POST['username']
-		passwd = request.POST['password']
+        user = authenticate(request, username=usrname, password=passwd)  # None
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        elif not User.objects.filter(username=usrname).exists():
+            messages.warning(
+                request, f'Please enter a correct username and password. Note that both fields may be case-sensitive.')
+            return redirect('login')
+        elif not User.objects.get(username=usrname).is_active:
+            usr = User.objects.get(username=usrname)
+            usr_otp = random.randint(100000, 999999)
+            UserOTP.objects.create(user=usr, otp=usr_otp)
+            mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
 
-		user = authenticate(request, username = usrname, password = passwd) #None
-		if user is not None:
-			login(request, user)
-			return redirect('profile')
-		elif not User.objects.filter(username = usrname).exists():
-			messages.warning(request, f'Please enter a correct username and password. Note that both fields may be case-sensitive.')
-			return redirect('login')
-		elif not User.objects.get(username=usrname).is_active:
-			usr = User.objects.get(username=usrname)
-			usr_otp = random.randint(100000, 999999)
-			UserOTP.objects.create(user = usr, otp = usr_otp)
-			mess = f"Hello {usr.first_name},\nYour OTP is {usr_otp}\nThanks!"
+            send_mail(
+                "Welcome to QP web - Verify Your Email",
+                mess,
+                'qpcom80@gmail.com',
+                [usr.email],
+                fail_silently=False
+            )
+            return render(request, 'accounts/login.html', {'otp': True, 'usr': usr})
+        else:
+            messages.warning(
+                request, f'Please enter a correct username and password. Note that both fields may be case-sensitive.')
+            return redirect('login')
 
-			send_mail(
-				"Welcome to QP web - Verify Your Email",
-				mess,
-				'qpcom80@gmail.com',
-				[usr.email],
-				fail_silently = False
-				)
-			return render(request, 'accounts/login.html', {'otp': True, 'usr': usr})
-		else:
-			messages.warning(request, f'Please enter a correct username and password. Note that both fields may be case-sensitive.')
-			return redirect('login')
-
-	context={
+    context = {
 
     }
-	return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/login.html', context)
+
 
 def profile(request):
-	my_papers=Provider.objects.filter(name=request.user)
-	my_recs=request.user.profile.get_recomended_profiles()
-	print("get request\n")
-	print(my_papers,my_recs)
-	context={
-	"my_papers":my_papers,
-	"my_recs":my_recs,
-	}
+    my_papers_reviewing = Provider.objects.filter(name=request.user)
+    my_papers_uploaded = Question_papers.objects.filter(provider=request.user)
+    print(my_papers_uploaded)
+    my_recs = request.user.profile.get_recomended_profiles()
+    print(my_recs)
+    context = {
+        "my_papers_reviewing": my_papers_reviewing,
+        "my_papers_uploaded": my_papers_uploaded,
+        "my_recs": my_recs,
+    }
 
-	return render(request, 'accounts/profile.html', context)
-
-
-
+    return render(request, 'accounts/profile.html', context)
--- question_papers/views.py
+++ question_papers/views.py
@@ -1,109 +1,125 @@
-from django.shortcuts import render, HttpResponse,redirect
-from .models import Question_papers,Issues,Provider
-from django.http import request,HttpRequest
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Question_papers, Issues, Provider
+from django.http import request, HttpRequest
 from django.contrib import messages
 from django.core.mail import send_mail
-from .forms import ProviderForm,issueForm
+from .forms import ProviderForm, issueForm
 from django.core import serializers
 from examination.models import Profile
 from django.contrib.auth.models import User
 # Create your views here.
 
+
 def filter_first_option(request):
-    college=Question_papers.objects.order_by('college').distinct('college')
-    qs_json=serializers.serialize('json',college)
-    return HttpResponse(qs_json,content_type='application/json')
+    college = Question_papers.objects.order_by('college').distinct('college')
+    qs_json = serializers.serialize('json', college)
+    return HttpResponse(qs_json, content_type='application/json')
+
 
 def colleges(request):
-    allqp=Question_papers.objects.order_by('college').distinct('college')
-    context={'allqp' : allqp,'Select':'Select Your Current Education : '}
-    return render(request,'question_papers/colleges.html',context)
+    allqp = Question_papers.objects.order_by('college').distinct('college')
+    context = {'allqp': allqp, 'Select': 'Select Your Current Education : '}
+    return render(request, 'question_papers/colleges.html', context)
 
-def college(request,college):
+
+def college(request, college):
     if college.startswith("json"):
-        college=college.split("-")[1]
-        college=Question_papers.objects.filter(college=college).order_by('university').distinct('university')
-        qs_json=serializers.serialize('json',college)
-        return HttpResponse(qs_json,content_type='application/json')
+        college = college.split("-")[1]
+        college = Question_papers.objects.filter(
+            college=college).order_by('university').distinct('university')
+        qs_json = serializers.serialize('json', college)
+        return HttpResponse(qs_json, content_type='application/json')
+
+    college = Question_papers.objects.filter(
+        college=college).order_by('university').distinct('university')
+    college = {'college': college}
+    return render(request, 'question_papers/universities.html', college)
 
 
-    college=Question_papers.objects.filter(college=college).order_by('university').distinct('university')
-    college={'college' : college}
-    return render(request,'question_papers/universities.html',college)
-
-def university(request,college,university):
+def university(request, college, university):
     if college.startswith("json"):
-        college=college.split("-")[1]
-        university=Question_papers.objects.filter(university=university,college=college).order_by('course').distinct('course')
-        qs_json=serializers.serialize('json',university)
-        return HttpResponse(qs_json,content_type='application/json')
-    university=Question_papers.objects.filter(university=university,college=college).order_by('course').distinct('course')
-    university={'university' : university}
-    return render(request,'question_papers/courses.html',university)
+        college = college.split("-")[1]
+        university = Question_papers.objects.filter(
+            university=university, college=college).order_by('course').distinct('course')
+        qs_json = serializers.serialize('json', university)
+        return HttpResponse(qs_json, content_type='application/json')
+    university = Question_papers.objects.filter(
+        university=university, college=college).order_by('course').distinct('course')
+    university = {'university': university}
+    return render(request, 'question_papers/courses.html', university)
 
-def course(request,college,university,course):
+
+def course(request, college, university, course):
     if college.startswith("json"):
-        college=college.split("-")[1]
-        course=Question_papers.objects.filter(course=course, university=university).order_by('year').distinct('year')
-        qs_json=serializers.serialize('json',course)
-        return HttpResponse(qs_json,content_type='application/json')
-    course=Question_papers.objects.filter(course=course, university=university).order_by('year').distinct('year')
-    course={'course' : course}
-    return render(request,'question_papers/classes.html',course)
+        college = college.split("-")[1]
+        course = Question_papers.objects.filter(
+            course=course, university=university).order_by('year').distinct('year')
+        qs_json = serializers.serialize('json', course)
+        return HttpResponse(qs_json, content_type='application/json')
+    course = Question_papers.objects.filter(
+        course=course, university=university).order_by('year').distinct('year')
+    course = {'course': course}
+    return render(request, 'question_papers/classes.html', course)
 
- 
-def year(request,college,university,course,year):
+
+def year(request, college, university, course, year):
     if college.startswith("json"):
-        college=college.split("-")[1]
-        year=Question_papers.objects.filter(course=course,university=university).order_by('subject').distinct('subject')
-        qs_json=serializers.serialize('json',year)
-        return HttpResponse(qs_json,content_type='application/json')
-    year=Question_papers.objects.filter(course=course,university=university).order_by('subject').distinct('subject')
-    year={'year':year}
-    return render(request,'question_papers/subjects.html',year)
+        college = college.split("-")[1]
+        year = Question_papers.objects.filter(
+            course=course, university=university).order_by('subject').distinct('subject')
+        qs_json = serializers.serialize('json', year)
+        return HttpResponse(qs_json, content_type='application/json')
+    year = Question_papers.objects.filter(
+        course=course, university=university).order_by('subject').distinct('subject')
+    year = {'year': year}
+    return render(request, 'question_papers/subjects.html', year)
 
-def question_papers(request,college,university,course,year,subject):
-    papers=Question_papers.objects.filter(subject=subject,university=university)
-    papers={'paper':papers}
-    return render(request,'question_papers/papers.html',papers)
+
+def question_papers(request, college, university, course, year, subject):
+    papers = Question_papers.objects.filter(
+        subject=subject, university=university)
+    papers = {'paper': papers}
+    return render(request, 'question_papers/papers.html', papers)
 
 
 def provider(request):
-    form=ProviderForm(request.GET)
-    form = ProviderForm(request.POST or None ,request.FILES )
+    form = ProviderForm(request.GET)
+    form = ProviderForm(request.POST or None, request.FILES)
     if form.is_valid():
-       
-        name=request.user
-        print(name)
-        paper_type=form.cleaned_data.get("paper_type") 
-        board=form.cleaned_data.get("board") 
-        claass = form.cleaned_data.get("claass") 
-        sem = form.cleaned_data.get("sem") 
-        sub= form.cleaned_data.get("sub") 
-        papertitle=form.cleaned_data.get("papertitle") 
-        doc = form.cleaned_data.get("doc") 
-        provide=Provider(paper_type=paper_type,name=name,board=board,claass=claass,sem=sem,sub=sub,papertitle=papertitle,doc=doc)
+
+        name = request.user
+        paper_type = form.cleaned_data.get("paper_type")
+        board = form.cleaned_data.get("board")
+        claass = form.cleaned_data.get("claass")
+        sem = form.cleaned_data.get("sem")
+        sub = form.cleaned_data.get("sub")
+        papertitle = form.cleaned_data.get("papertitle")
+        doc = form.cleaned_data.get("doc")
+        provide = Provider(paper_type=paper_type, name=name, board=board,
+                           claass=claass, sem=sem, sub=sub, papertitle=papertitle, doc=doc)
         provide.save()
-        messages.success(request, 'Thank you, we will check and update it soon .')
-        form=ProviderForm()
-        
-    providers={
-        'form':ProviderForm
+        messages.success(
+            request, 'Thank you, we will check and update it soon .')
+        form = ProviderForm()
+
+    providers = {
+        'form': ProviderForm
     }
-    
-    return render(request,'question_papers/provide.html',providers
-    )
+
+    return render(request, 'question_papers/provide.html', providers
+                  )
 
 
 def filters(request):
-    college=request.POST.get('college')
-    university=request.POST.get('university')
-    course=request.POST.get('course')
-    year=request.POST.get('year')
-    subject=request.POST.get('subject')
-    filter_obj=Question_papers.objects.filter(college=college).filter(year=year).filter(course=course).filter(subject=subject).filter(university=university)
-    filter_obj={'context':filter_obj}
-    return render(request,'question_papers/filter.html',filter_obj)
+    college = request.POST.get('college')
+    university = request.POST.get('university')
+    course = request.POST.get('course')
+    year = request.POST.get('year')
+    subject = request.POST.get('subject')
+    filter_obj = Question_papers.objects.filter(college=college).filter(year=year).filter(
+        course=course).filter(subject=subject).filter(university=university)
+    filter_obj = {'context': filter_obj}
+    return render(request, 'question_papers/filter.html', filter_obj)
 
 
 def issues(request):
@@ -111,97 +127,91 @@ def issues(request):
     if form.is_valid():
         form.save()
         messages.success(request, 'Your Message has been succesfully sent.')
-        form=issueForm()   
-    Issues={'form':issueForm}
-    return render(request,'question_papers/contact.html',Issues)
+        form = issueForm()
+    Issues = {'form': issueForm}
+    return render(request, 'question_papers/contact.html', Issues)
 
 
 def push(request):
-    pushes=Provider.objects.all()
-    push={'pushes':pushes}
-    return render(request,'question_papers/push.html',push)
+    pushes = Provider.objects.all()
+    push = {'pushes': pushes}
+    return render(request, 'question_papers/push.html', push)
+
 
 def pushed(request):
-    if request.method=='POST':
-        print("posting")
-        # provider=request.POST['pro']
-        provider=request.POST['pro']
-        college=request.POST['college']
-        university=request.POST['university']
-        course=request.POST['course']
-        year=request.POST['semister']
-        subject=request.POST['subject']
-        examination=request.POST['examination']
-        paper=request.POST['paper']
-        date=request.POST['date']
-        id=request.POST['id']
-        emailid=request.POST['email']
-        push=Question_papers(provider=provider,college=college,university=university,course=course,year=year,subject=subject,examination=examination,paper=paper)
+    if request.method == 'POST':
+        provider = request.POST['pro']
+        college = request.POST['college']
+        university = request.POST['university']
+        course = request.POST['course']
+        year = request.POST['semister']
+        subject = request.POST['subject']
+        examination = request.POST['examination']
+        paper = request.POST['paper']
+        date = request.POST['date']
+        id = request.POST['id']
+        emailid = request.POST['email']
+        push = Question_papers(provider=provider, college=college, university=university,
+                               course=course, year=year, subject=subject, examination=examination, paper=paper)
         push.save()
 
-        pull=Provider(id=id,name=Provider,email=emailid,level=college,board=university,claass=course,sem=year,sub=subject,papertitle=examination,doc=paper,provide_date=date)
+        pull = Provider(id=id, name=Provider, email=emailid, level=college, board=university,
+                        claass=course, sem=year, sub=subject, papertitle=examination, doc=paper, provide_date=date)
         pull.delete()
-        
+
         send_mail(
             'hello',
             'i am vignesh',
             'qpcom80@gmail.com',
             [emailid],
             fail_silently=True,
-            
+
         )
-        
 
+        return render(request, 'question_papers/push.html',)
 
-        return render(request,'question_papers/push.html',)
 
 def search(request):
-    query=request.GET['query']
-    query=query.lower()
-    print(query)
-    query=query.replace(" ","_")
-    print(query)
-    result=Question_papers.objects.filter(slug__icontains=query)
-    result={'results':result}
-    print(result)
-    return render(request,'question_papers/search.html',result)
+    query = request.GET['query']
+    query = query.lower()
+    query = query.replace(" ", "_")
+    result = Question_papers.objects.filter(slug__icontains=query)
+    result = {'results': result}
+    return render(request, 'question_papers/search.html', result)
+
 
 def profile_settings(request):
-    if request.method=="POST" or None or request.FILES:
+    if request.method == "POST" or None or request.FILES:
 
-        user_id=request.POST.get('user_id')
-        name=request.POST.get('full_name')
-        pic=request.FILES.get('pic')
-        bio=request.POST.get('bio')
-        college=request.POST.get('college')
+        user_id = request.POST.get('user_id')
+        name = request.POST.get('full_name')
+        pic = request.FILES.get('pic')
+        bio = request.POST.get('bio')
+        college = request.POST.get('college')
         try:
-            if name is "" or bio is  "" or college == "":
+            if name is "" or bio is "" or college == "":
                 messages.warning(request, 'Values can not be null.')
-                return render(request,'accounts/profile_edit.html')
+                return render(request, 'accounts/profile_edit.html')
 
-            userObj=User.objects.get(id=user_id)
-            print(userObj)
-            userObj.first_name=name
+            userObj = User.objects.get(id=user_id)
+            userObj.first_name = name
             userObj.save()
-            
-            profileObj=Profile.objects.get(user=request.user)
-        
-            profileObj.college=college
+
+            profileObj = Profile.objects.get(user=request.user)
+
+            profileObj.college = college
             if pic is not None:
-                profileObj.pic=pic
-            profileObj.bio=bio
+                profileObj.pic = pic
+            profileObj.bio = bio
             profileObj.save()
             messages.success(request, 'Your profile updated successfully.')
 
             return redirect("profile")
         except Exception as e:
-            messages.error(request,f"Profile not updated successfully\n {e}")
-            return render(request,'accounts/profile_edit.html')
+            messages.error(request, f"Profile not updated successfully\n {e}")
+            return render(request, 'accounts/profile_edit.html')
 
-    return render(request,'accounts/profile_edit.html')
-
-    
-
+    return render(request, 'accounts/profile_edit.html')
 
     # TWITTER_ENDPOINT = 'https://twitter.com/intent/tweet?text=%s'
 # FACEBOOK_ENDPOINT = 'https://www.facebook.com/sharer/sharer.php?u=%s'
