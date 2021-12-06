1d56db5de3be5b6a23565139572790cac7faf955
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-12-06 08:45:50 +0530

    email settings updated

--- MultipleSettings/development.py
+++ MultipleSettings/development.py
@@ -147,11 +147,12 @@ USE_L10N = True
 USE_TZ = True
 
 #Email Configarations
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='qpcom80@gmail.com'
-EMAIL_HOST_PASSWORD='vigneshqpweb'
+EMAIL_HOST_PASSWORD='yvdgeddmxtfbzwcg'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
