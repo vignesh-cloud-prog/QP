94188124415b5d084e119ebe3b887aa1b70f1cf8
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-12-09 20:25:03 +0530

    env updated

--- MultipleSettings/herokusettings.py
+++ MultipleSettings/herokusettings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='qpcom80@gmail.com'
-EMAIL_HOST_PASSWORD='yvdgeddmxtfbzwcg'
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
